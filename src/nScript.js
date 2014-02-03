aa39e9d2070be8c72680d48174cbfdabebf76493
njean <iru@nate.com>
2014-02-03 18:34:29 +0900

    add ua
    add ua

--- src/nScript.js
+++ src/nScript.js
@@ -9,6 +9,8 @@
 		isMobile : (_ua.indexOf("MOBILE") > -1)
 		// @ * Mobile	| Android
 		// @ * * Samsung | Galaxy
+		// @ * * GalaxyS4 Ally
+		,isGals4 : (_ua.indexOf("SHV-E300") > -1 && _ua.indexOf("SHV-E330"))
 		// @ * * GalaxyS4 LTE
 		,isGals4-lg : (_ua.indexOf("SHV-E300L") > -1)
 		,isGals4-kt : (_ua.indexOf("SHV-E300K") > -1)
@@ -19,7 +21,6 @@
 		,isGals4a-sk : (_ua.indexOf("SHV-E330S") > -1)
 		// @ * Mobile	| Apple
 		,isIphone : (_ua.indexOf("IPHONE") > -1)
-
 		// @ * Pad&Tab	| Android
 		,isNexus : (_ua.indexOf("NEXUS") > -1)
 		,isGalaxyTab : (_ua.indexOf("SHW-M180") > -1)
@@ -28,7 +29,18 @@
 
 		// @ PC WebBrowser
 		// @ * MS Explorer
+		,isIE: ($.browser.msie)?true:false
+		,isIE10: (_ua.indexOf('MSIE 10')>-1)
+		,isIE9: (_ua.indexOf('MSIE 9')>-1)
+		,isIE8: (_ua.indexOf('MSIE 9')==-1&&__ua.indexOf('MSIE 8')>-1)
+		,isIE7: (_ua.indexOf('MSIE 9')==-1&&__ua.indexOf('MSIE 8')==-1&&__ua.indexOf('MSIE 7')>-1)
 		// @ * Apple Safari
+		,isSF: (_ua.indexOf('SAFARI')>-1&&__ua.indexOf('CHROME')==-1)
 		// @ * Google Chrome
+		,isCR: (_ua.indexOf('CHROME')>-1)
+		// @ * Opera
+		,isOP: (_ua.indexOf('OPERA')>-1)
+		// @ * Mozilla FireFox
+		,isFF: (_ua.indexOf('FIREFOX')>-1)
 	});
 })(jQuery);
