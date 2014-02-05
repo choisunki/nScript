70eb0957dca39b151af8d70f35ce93f2a631e4a3
choisunki <iru@nate.com>
2014-02-06 00:25:41 +0900

    문법오류 제거
    문법오류 제거

--- src/nScript.js
+++ src/nScript.js
@@ -10,15 +10,15 @@
 		// @ * Mobile	| Android
 		// @ * * Samsung | Galaxy
 		// @ * * GalaxyS4 Ally
-		,isGals4 : (_ua.indexOf("SHV-E300") > -1 && _ua.indexOf("SHV-E330"))
+		,isGals4 : (_ua.indexOf("SHV-E300") > -1 && _ua.indexOf("SHV-E330") > -1)
 		// @ * * GalaxyS4 LTE
-		,isGals4-lg : (_ua.indexOf("SHV-E300L") > -1)
-		,isGals4-kt : (_ua.indexOf("SHV-E300K") > -1)
-		,isGals4-sk : (_ua.indexOf("SHV-E300S") > -1)
+		,isGals4lg : (_ua.indexOf("SHV-E300L") > -1)
+		,isGals4kt : (_ua.indexOf("SHV-E300K") > -1)
+		,isGals4sk : (_ua.indexOf("SHV-E300S") > -1)
 		// @ * * GalaxyS4 LTE A
-		,isGals4a-lg : (_ua.indexOf("SHV-E330L") > -1)
-		,isGals4a-kt : (_ua.indexOf("SHV-E330K") > -1)
-		,isGals4a-sk : (_ua.indexOf("SHV-E330S") > -1)
+		,isGals4alg : (_ua.indexOf("SHV-E330L") > -1)
+		,isGals4akt : (_ua.indexOf("SHV-E330K") > -1)
+		,isGals4ask : (_ua.indexOf("SHV-E330S") > -1)
 		// @ * Mobile	| Apple
 		,isIphone : (_ua.indexOf("IPHONE") > -1)
 		// @ * Pad&Tab	| Android
@@ -32,10 +32,10 @@
 		,isIE: ($.browser.msie)?true:false
 		,isIE10: (_ua.indexOf('MSIE 10')>-1)
 		,isIE9: (_ua.indexOf('MSIE 9')>-1)
-		,isIE8: (_ua.indexOf('MSIE 9')==-1&&__ua.indexOf('MSIE 8')>-1)
-		,isIE7: (_ua.indexOf('MSIE 9')==-1&&__ua.indexOf('MSIE 8')==-1&&__ua.indexOf('MSIE 7')>-1)
+		,isIE8: (_ua.indexOf('MSIE 9')==-1&&_ua.indexOf('MSIE 8')>-1)
+		,isIE7: (_ua.indexOf('MSIE 9')==-1&&_ua.indexOf('MSIE 8')==-1&&_ua.indexOf('MSIE 7')>-1)
 		// @ * Apple Safari
-		,isSF: (_ua.indexOf('SAFARI')>-1&&__ua.indexOf('CHROME')==-1)
+		,isSF: (_ua.indexOf('SAFARI')>-1&&_ua.indexOf('CHROME')==-1)
 		// @ * Google Chrome
 		,isCR: (_ua.indexOf('CHROME')>-1)
 		// @ * Opera
